a496091ddd641638c9bea6539a244da4e10f8943
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-05-06 15:42:00 +0100

    Updated tags for ally

--- src/test/functional/tests/accessibility.spec.ts
+++ src/test/functional/tests/accessibility.spec.ts
@@ -1,34 +1,34 @@
 import { test } from '../fixtures';
 import { withCreatedCourt } from '../helpers/testSupport';
 
-test.describe('Accessibility Tests', () => {
-  test(
-    'Home Page Accessibility',
-    {
-      tag: '@a11y',
-    },
-    async ({ homePage, axeUtils }) => {
+test.describe(
+  'Accessibility Tests',
+  {
+    tag: '@a11y',
+  },
+  () => {
+    test('Home Page Accessibility', async ({ homePage, axeUtils }) => {
       await homePage.expectVisibleElements();
       await axeUtils.audit();
-    }
-  );
+    });
 
-  test('Court Edit Page Accessibility', async ({ axeUtils, courtEditPage, playwright }) => {
-    await withCreatedCourt(
-      playwright,
-      'Court Edit Accessibility Test',
-      { serviceCenter: false },
-      async ({ createdCourt }) => {
-        await courtEditPage.goto(createdCourt.id);
-        await courtEditPage.expectVisibleElements();
-        await axeUtils.audit();
-      }
-    );
-  });
+    test('Court Edit Page Accessibility', async ({ axeUtils, courtEditPage, playwright }) => {
+      await withCreatedCourt(
+        playwright,
+        'Court Edit Accessibility Test',
+        { serviceCenter: false },
+        async ({ createdCourt }) => {
+          await courtEditPage.goto(createdCourt.id);
+          await courtEditPage.expectVisibleElements();
+          await axeUtils.audit();
+        }
+      );
+    });
 
-  test('Court Not Found Page Accessibility', async ({ axeUtils, courtEditPage }) => {
-    await courtEditPage.goto('not-a-uuid');
-    await courtEditPage.expectVisibleElements();
-    await axeUtils.audit();
-  });
-});
+    test('Court Not Found Page Accessibility', async ({ axeUtils, courtEditPage }) => {
+      await courtEditPage.goto('not-a-uuid');
+      await courtEditPage.expectVisibleElements();
+      await axeUtils.audit();
+    });
+  }
+);
