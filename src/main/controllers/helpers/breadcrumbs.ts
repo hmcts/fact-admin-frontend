bb85239870db95a2b6f6b3d9a88cba78e4a6b2a5
debolina-seal <debolina.seal@gmail.com>
2026-06-30 13:41:25 +0100

    breadcrumb initial commit

---
+++ src/main/controllers/helpers/breadcrumbs.ts
@@ -0,0 +1,33 @@
+export type BreadcrumbItem = {
+  href?: string;
+  text: string;
+};
+
+export function buildEditBreadcrumbs(courtId: string, courtName: string, currentPage = 'Edit'): BreadcrumbItem[] {
+  return [
+    { href: '/', text: 'Home' },
+    { href: `/courts/${courtId}/edit`, text: courtName },
+    { text: currentPage , href: '#' },
+  ];
+}
+
+export function buildSectionBreadcrumbs(
+  courtId: string,
+  courtName: string,
+  sectionText: string,
+  sectionPath: string,
+  currentPage?: string
+): BreadcrumbItem[] {
+  const breadcrumbs: BreadcrumbItem[] = [
+    { href: '/', text: 'Home' },
+    { href: `/courts/${courtId}/edit`, text: courtName },
+    { href: `/courts/${courtId}/edit/${sectionPath}`, text: sectionText },
+  ];
+
+  if (currentPage) {
+    breadcrumbs.push({ text: currentPage, href: '#' });
+  }
+
+  return breadcrumbs;
+}
+
