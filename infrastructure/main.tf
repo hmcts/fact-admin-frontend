b0a9f9341c2896c881d3076820b69dd462ca2343
debolina-seal <100166232+debolina-seal@users.noreply.github.com>
2026-07-14 16:06:46 +0100

    Clean up unused Azure resources in main.tf
    Removed unused data sources and access policies for Azure resources.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,31 +9,6 @@ data "azurerm_key_vault" "app_kv" {
   resource_group_name = local.key_vault_resource_group_name
 }
 
-data "azurerm_client_config" "current" {}
-
-data "azurerm_user_assigned_identity" "jenkins_preview" {
-  count               = var.env == "aat" ? 1 : 0
-  name                = "jenkins-preview-mi"
-  resource_group_name = "managed-identities-preview-rg"
-}
-
-data "azurerm_key_vault" "bootstrap" {
-  count               = var.env == "aat" ? 1 : 0
-  name                = "fact-bstrap-${var.env}-kv"
-  resource_group_name = "fact-bstrap-${var.env}-rg"
-}
-
-resource "azurerm_key_vault_access_policy" "jenkins_preview" {
-  count        = var.env == "aat" ? 1 : 0
-  key_vault_id = data.azurerm_key_vault.bootstrap[0].id
-  object_id    = data.azurerm_user_assigned_identity.jenkins_preview[0].principal_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-
-  key_permissions         = ["Get", "List"]
-  certificate_permissions = ["Get", "List"]
-  secret_permissions      = ["Get", "List"]
-}
-
 data "azurerm_resource_group" "fact_rg" {
   name = "${var.product}-${var.env}"
 }
