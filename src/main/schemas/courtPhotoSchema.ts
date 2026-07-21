04a19daf19e73ad3a40b4b2de1a85cc7fcd4029f
hurricanepilot <hurricanepilot@gmail.com>
2026-07-21 12:41:27 +0100

    Fix caching issues with statically named images.

--- src/main/schemas/courtPhotoSchema.ts
+++ src/main/schemas/courtPhotoSchema.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
-export const courtPhotoSchema = z.object({
-  id: z.uuid(),
-  courtId: z.uuid(),
-  fileLink: z.string().optional().nullable(),
-  lastUpdatedAt: z.string(),
-  updatedByUserId: z.uuid().optional().nullable(),
-});
+export const courtPhotoSchema = z
+  .object({
+    id: z.uuid(),
+    courtId: z.uuid(),
+    fileLink: z.string().optional().nullable(),
+    lastUpdatedAt: z.string(),
+    updatedByUserId: z.uuid().optional().nullable(),
+  })
+  .transform(courtPhoto => ({
+    // this is done to ensure that the photo link is unique. Azure's blob
+    // storage will ignore the odd param in the request
+    fileLink: courtPhoto.fileLink + '?' + crypto.randomUUID(),
+  }));
 
 export type CourtPhoto = z.infer<typeof courtPhotoSchema>;
