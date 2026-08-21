c78f1d471d7790c7c8dddc74eea099fa7275e7ec
ibrahimelsanosi-moj <ibrahim.elsanosi@justice.gov.uk>
2026-08-21 15:33:58 +0100

    FACT-2912 fix into test fail when data api is not running

--- src/test/routes/info.test.ts
+++ src/test/routes/info.test.ts
@@ -1,3 +1,4 @@
+import { InfoContributor } from '@hmcts/info-provider/';
 import { HttpStatusCode } from 'axios';
 import { restore, stub } from 'sinon';
 import request from 'supertest';
@@ -12,6 +13,7 @@ describe('Info route', () => {
 
   test('returns info payload for GET /info', async () => {
     stub(OperationsApi.prototype, 'checkHealth').resolves(true);
+    stub(InfoContributor.prototype, 'call').resolves({ status: 'UP' } as never);
 
     const response = await request(app).get('/info');
 
@@ -22,6 +24,7 @@ describe('Info route', () => {
 
   test('includes dataApiUp=true when the Data API health check succeeds', async () => {
     stub(OperationsApi.prototype, 'checkHealth').resolves(true);
+    stub(InfoContributor.prototype, 'call').resolves({ status: 'UP' } as never);
 
     const response = await request(app).get('/info');
 
@@ -32,6 +35,7 @@ describe('Info route', () => {
 
   test('includes dataApiUp=false when the Data API health check fails', async () => {
     stub(OperationsApi.prototype, 'checkHealth').resolves(false);
+    stub(InfoContributor.prototype, 'call').resolves({ status: 'DOWN' } as never);
 
     const response = await request(app).get('/info');
 
