85b1cebcb4f04e036c9bdbd7f5e8b74d4292cd8d
Jon <hurricanepilot@gmail.com>
2026-09-02 11:41:41 +0100

    FACT-2908 - update package deps (#317)
    * initial update for minor increments
    
    * update webpack and plugins
    
    * update `config` to latest, remove redundant @types/config dep and fix tests
    
    This involved removing the @types/config import as it's now bundled and fixing tests by including the config mjs module as part of the jest configuration for babel transformation
    
    * update @types/node and @types/supertest to latest
    
    * Update awilix to latest
    
    * update sinon and pa11y to latest versions
    
    * update redis to latest version
    
    * update codeceptjs to the latest version
    
    * update the moj frontend libary to the latest version
    
    * upgraded to typescript 6.0.3 / config malarkey to fix webpack/jest
    
    * removed atomic resolution overrides (protobufjs, uuid, ip-address, form-data, joi)
    
    * remove body-parser and js-yaml resolution overrides.
    
    * add additional inclusions to the docker build for tsconfig updates
    
    * update jest configs and prune unnecessary configs.
    
    * remove ranges from all package.json version includes
    
    * yarn lock update as a result of yarn install with less resolution overrides

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>/src/test/a11y'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    '^.+\\.(js|mjs)$': 'babel-jest',
   },
-  transformIgnorePatterns: ['/node_modules/(?!chai|uuid)'],
+  transformIgnorePatterns: ['/node_modules/(?!chai|uuid|config)'],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
+const baseConfig = require('./jest.base.config');
+
 module.exports = {
+  ...baseConfig,
   roots: ['<rootDir>/src/test/unit'],
-  testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!chai|uuid)'],
   modulePathIgnorePatterns: ['<rootDir>/src/test/unit/mocks'],
 };
