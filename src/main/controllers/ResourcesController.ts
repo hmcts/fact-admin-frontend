8e76c12f206b8c3e92cdb19081a3a25de8b52def
hurricanepilot <hurricanepilot@gmail.com>
2026-09-17 13:04:37 +0100

    Add proxy URLs for serving images/csv from data-api

---
+++ src/main/controllers/ResourcesController.ts
@@ -0,0 +1,61 @@
+import { GET, route } from 'awilix-express';
+import { Request, Response } from 'express';
+
+import { OperationsApi } from '../requests/OperationsApi';
+
+import BaseController from './BaseController';
+
+@route('/res')
+export default class ResourcesController extends BaseController {
+  constructor(private readonly operationsApi = new OperationsApi()) {
+    super();
+  }
+
+  @route('/img/:courtId')
+  @GET()
+  public async img(req: Request, res: Response): Promise<void> {
+    const courtId = this.getUuidRouteParam(req, 'courtId');
+
+    if (!courtId) {
+      res.sendStatus(400);
+      return;
+    }
+
+    return this.serveFileStream(`/courts/${courtId}/v1/photo/image`, res);
+  }
+
+  @route('/csv')
+  @GET()
+  public async csv(req: Request, res: Response): Promise<void> {
+    return this.serveFileStream('/csv/', res);
+  }
+
+  private async serveFileStream(url: string, res: Response): Promise<void> {
+    const result = await this.operationsApi.getFileStream(url);
+
+    if (typeof result === 'number') {
+      res.sendStatus(result);
+      return;
+    }
+
+    if (result.headers.contentType) {
+      res.setHeader('Content-Type', result.headers.contentType);
+    }
+    if (result.headers.contentDisposition) {
+      res.setHeader('Content-Disposition', result.headers.contentDisposition);
+    }
+    if (result.headers.contentLength) {
+      res.setHeader('Content-Length', result.headers.contentLength);
+    }
+
+    result.stream.on('error', () => {
+      if (!res.headersSent) {
+        res.sendStatus(502);
+      } else {
+        res.destroy();
+      }
+    });
+
+    result.stream.pipe(res);
+  }
+}
